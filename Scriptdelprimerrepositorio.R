0b2cbc6687ec71686aed63e8d359303c4a07b0d5
pinangodiana <pinangodiana@gmail.com>
2023-03-16 17:20:45 -0600

    en esta version cargue las imagenes

---
+++ Scriptdelprimerrepositorio.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+
+data("iris")
+ggplot(iris, aes(Petal.Length, Petal.Width))+
+  geom_point(aes(color=Species, shape= Species))
+
+ggsave("iris.pdf")
+
+write.csv(iris, "iris.csv")
+
+install.packages('rmarkdown')
